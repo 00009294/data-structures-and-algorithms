6a3fa7d98215885025c2cc6e55a2a697a499207d
FayzullaevJavlonbek <javlonbek.fayzullaev@webase.uz>
2025-11-29 13:34:22 +0500

    update-linked-list

--- docs/linked-lists/code-examples/LinkedList/LinkedList/Models/SinglyLinkedListNode.cs
+++ docs/linked-lists/code-examples/LinkedList/LinkedList/Models/SinglyLinkedListNode.cs
@@ -8,13 +8,126 @@
     /// </summary>
     public class SinglyLinkedListNode
     {
-        public int Value { get; set; }
-        public SinglyLinkedListNode? Next { get; set; }
-
-        public SinglyLinkedListNode(int value = 0, SinglyLinkedListNode? next = null)
+        /// <summary>
+        /// LinkedList ni data strukturasi
+        /// </summary>
+        public class LinkedList
         {
-            Value = value;
-            Next = next;
+            public int Value { get; set; }
+            public LinkedList? Next { get; set; }
+           
+            public LinkedList(int value = 0, LinkedList? next = null)
+            {
+                Value = value;
+                Next = next;
+            }
+        }
+
+        /// <summary>
+        /// Qutilarni bo'shi
+        /// </summary>
+        private LinkedList? Head { get; set; }
+        
+        /// <summary>
+        /// Qutilarni jami soni
+        /// </summary>
+        private int CurrentSize { get; set; }
+
+        public SinglyLinkedListNode()
+        {
+            Head = null;
+            CurrentSize = 0;
+        }
+
+        /// <summary>
+        /// Bog'langan qutilarni boshiga qoshish
+        /// </summary>
+        /// <param name="newNode"> yangi quti </param>
+        public void AddFront(LinkedList newNode)
+        {
+            if (!IsValid(newNode))
+                return;
+
+            newNode.Next = Head;
+            Head = newNode;
+
+            CurrentSize++;
+        }
+
+        /// <summary>
+        /// Bog'langan qutilarni ohiriga qoshish
+        /// </summary>
+        /// <param name="newNode"> yangi quti </param>
+        public void AddBack(LinkedList newNode)
+        {
+            if(!IsValid(newNode))
+                return;
+
+            if (!IsValid(Head))
+            {
+                Head = newNode;
+                CurrentSize++;
+            }
+
+            LinkedList curr = Head!;
+            do
+            {
+                curr = curr.Next!;
+            }
+            while (curr.Next != null);
+
+            curr.Next = newNode;
+            CurrentSize++;
+        }
+
+        /// <summary>
+        /// Tartib raqam boyicha bog'langan qutilarga yangi quti qo'shish
+        /// </summary>
+        /// <param name="newNode"> yangi quti </param>
+        /// <param name="index"> tartib raqam </param>
+        public void AddMiddle(LinkedList newNode, int index)
+        {
+            if(!IsValid(newNode) || index > CurrentSize) 
+                return;
+
+            if(index == 0 || Head is null)
+            {
+                AddFront(newNode);
+                return;
+            }
+            else if(index == CurrentSize-1)
+            {
+                AddBack(newNode);
+                return;
+            }
+
+            LinkedList curr = Head;
+            int i = 0;
+            while (index - 1 != i && curr != null)
+            {
+                curr = curr.Next!;
+                i++;
+            }
+
+            if (!IsValid(curr))
+                return;
+
+            LinkedList next = curr.Next!;
+            curr.Next = newNode;
+            newNode.Next = next;
+            CurrentSize++;
+        }
+
+        /// <summary>
+        /// TEKSHIRUV: Quti bosh yoki Value 0 dan kichik bo'lish mumkin emas
+        /// </summary>
+        /// <param name="node"> oddiy quti </param>
+        public static bool IsValid(LinkedList? node)
+        {
+            if (node is null || node.Value < 0)
+                return false;
+
+            return true;
         }
     }
 }
