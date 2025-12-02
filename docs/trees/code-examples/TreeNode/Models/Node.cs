a772eac6b09a099b0d3948745361a279b8f3f54a
FayzullaevJavlonbek <javlonbek.fayzullaev@webase.uz>
2025-12-02 19:34:12 +0500

    add-tree-node-solution

---
+++ docs/trees/code-examples/TreeNode/Models/Node.cs
@@ -0,0 +1,26 @@
+﻿namespace TreeNode.Models
+{
+    public class Node
+    {
+        public class NodeItem
+        {
+            public int Value { get; set; }
+            public NodeItem? Left { get; set; }
+            public NodeItem? Right { get; set; }
+
+            public NodeItem(int value = 0, NodeItem? left = null, NodeItem? right = null)
+            {
+                Value = value;
+                Left = left;
+                Right = right;
+            }
+        }
+
+        public NodeItem? Root { get; set; }
+
+        public Node()
+        {
+            Root = null;
+        }
+    }
+}
